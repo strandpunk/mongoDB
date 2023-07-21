ab44c156011d73433eec2ce8386f894580f4d219
Anatoliy <strandpunk@yahoo.com>
2023-07-21 14:05:54 +0300

    Log users in and out

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -3,6 +3,8 @@ const User = require('../models/userModel')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
+// register
+
 router.post('/', async (req, res) => {
     try {
         const { email, password, passwordVerify } = req.body
@@ -54,13 +56,68 @@ router.post('/', async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true
         })
-        .send()
-
+            .send()
 
     } catch (error) {
         consle.error(error)
         res.sendStatus(500).send()
     }
+})
+
+
+// log in
+
+router.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body
+
+        // validation
+
+        if (!email || !password)
+            return res
+                .status(400)
+                .json({ errorMessage: 'Please enter all required fields.' });
+
+        const existingUser = await User.findOne({ email })
+        if (!existingUser)
+            return res
+                .status(401)
+                .json({ errorMessage: 'Wrong email or password.' });
+
+
+        const passwordCorrect = await bcrypt.compare(password, existingUser.passwordHash)
+        if (!passwordCorrect)
+            return res
+                .status(401)
+                .json({ errorMessage: 'Wrong email or password.' });
+
+
+        // sign the token
+
+        const token = jwt.sign({
+            user: existingUser._id
+        }, process.env.JWT_SECRET)
+
+        // send the token in a HTTP-only cookie
+
+        res.cookie('token', token, {
+            httpOnly: true
+        })
+            .send()
+
+
+    } catch (error) {
+        console.error(error)
+        res.sendStatus(500).send()
+    }
+})
+
+router.get('/logout', (req, res) => {
+    res.cookie('token', "", {
+        httpOnly: true,
+        expires: new Date(0)
+    })
+        .send()
 })
 
 module.exports = router
