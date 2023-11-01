886154d2bd86c9fe2021bd0c79ba614f5fe936e8
Strandpunk <strandpunk@yahoo.com>
2023-11-01 20:16:17 +0300

    css update

--- client/src/data/Data.js
+++ client/src/data/Data.js
@@ -1,32 +1,44 @@
-import DataForm from "./DataForm"
-import DataList from "./DataList"
-import axios from "axios"
-import { useEffect, useState } from "react"
-
-
+import DataForm from "./DataForm";
+import DataList from "./DataList";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./Data.css";
 
 function Data() {
-    const [data, setData] = useState([])
+  const [data, setData] = useState([]);
 
-    async function getData() {
-        const dataList = await axios.get('http://localhost:5000/data/')
-        setData(dataList.data)
-    }
+  async function getData() {
+    const dataList = await axios.get("http://localhost:5000/data/");
+    setData(dataList.data);
+  }
 
+  useEffect(() => {
+    getData();
+  }, []);
 
-    useEffect(() => {
-        getData()
-    }, [])
+  console.log(data);
 
-    return (
-        <div>
-            <div>
-                <DataForm getData={getData} />
-            </div>
-            <div>
-                <DataList data={data} />
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      {data.length != 0 && (
+        <>
+          <div>
+            <DataForm getData={getData} />
+          </div>
+          <div>
+            <DataList data={data} />
+          </div>
+        </>
+      )}
+      {data.length == 0 && (
+        <>
+          <div>
+            <DataForm getData={getData} />
+          </div>
+          <div className="nodata">У вас пока нет записей</div>
+        </>
+      )}
+    </div>
+  );
 }
-export default Data
+export default Data;
