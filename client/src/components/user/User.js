b64f5439935ca03982ced17dd9697cf7fe137320
strandpunk <strandpunk@yahoo.com>
2024-03-11 01:01:14 +0300

    update

--- client/src/components/user/User.js
+++ client/src/components/user/User.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 import { useEffect, useRef, useState } from "react";
 import "./User.css";
+import { motion } from 'framer-motion';
 const moment = require('moment');
 
 function User({ samuraiInfo, setSamuraiInfo }) {
   const [user, setUser] = useState({});
+  const [isSamurai, setIsSamurai] = useState(false);
 
   async function getUser() {
     try {
@@ -15,24 +17,18 @@ function User({ samuraiInfo, setSamuraiInfo }) {
     }
   }
 
-  //--for--fun--
   useEffect(() => {
-    // Создаём переменную для хранения id таймера
-    let timerId;
     if (samuraiInfo === true) {
-      // Устанавливаем таймер
-      timerId = setTimeout(() => {
-        // По истечении времени меняем значение переменной
+      setIsSamurai(true);
+      setTimeout(() => {
         setSamuraiInfo(false);
+        setIsSamurai(false);
       }, 1000);
     }
-  
-    // Возвращаем функцию очистки таймера для предотвращения утечки памяти
-    return () => clearTimeout(timerId);
-  }, [samuraiInfo, setSamuraiInfo]); // Зависимость только от samuraiInfo
-  //------------
+  }, [samuraiInfo, setSamuraiInfo]);
 
-  
+
+
   useEffect(() => {
     getUser();
     // getImage();
@@ -78,25 +74,37 @@ function User({ samuraiInfo, setSamuraiInfo }) {
   //   setAllImages(result.data.data);
   // };
   //----------------------------------------------------------
-
   return (
     <div>
       <div className="user__info">
         {user.name ? (
           <>
-            {samuraiInfo === true ? (<>
+            <motion.h1
+              initial={{ opacity: 0 }} // Начальное значение прозрачности
+              animate={{ opacity: isSamurai ? 1 : 1 }} // Анимация появления текста
+              transition={{ duration: 0.5 }} // Длительность анимации
+            >
+              {isSamurai ? "Never fade away" : `Пользователь ${user.name}`}
+            </motion.h1>
+            <br></br>
+            <div className="user__wrapper">
+              <motion.div
+                initial={{ opacity: 0 }} // Начальное значение прозрачности
+                animate={{ opacity: isSamurai ? 1 : 1 }} // Анимация появления текста
+                transition={{ delay: 0.2, duration: 0.5 }} // Задержка перед анимацией и длительность анимации
+                className="user__wrapper-text"
+              >
 
-              <h1>Never fade away</h1>
-              <br></br>
-              <div className="user__wrapper">
-                <div className="user__wrapper-text">
-                  <div>Дата подписки: 20-aug-2023 04:45</div>
-                  <div>Email: silverhand@yandex.ru</div>
-                  <div>Тип темперамента: silverhand</div>
-                  <div>Дата создания аккаунта: 20-aug-2023 04:45</div>
-                  <div>Аватар: JOHNI-SILVERHAND-20aug2023-Q.jpg</div>
-                </div>
-                <img
+                <div>Дата подписки: {isSamurai ? "3 aug 2077" : moment(user.subDate).format('DD-MMM-YYYY HH:mm')}</div>
+                <div>Email: {isSamurai ? "silverhand@yandex.ru" : user.email}</div>
+                <div>Тип темперамента: {isSamurai ? "silverhand" : user.temperament}</div>
+                <div>Дата создания аккаунта: {isSamurai ? "3 aug 2077" : moment(user.createdAt).format('DD-MMM-YYYY HH:mm')}</div>
+                <div>Аватар: {isSamurai ? "JOHNI-SILVERHAND-20aug2023-Q.jpg" : user.avatar}</div>
+
+
+              </motion.div>
+              {isSamurai ? (
+                <motion.img
                   style={{
                     height: "400px",
                     width: "250px",
@@ -105,22 +113,10 @@ function User({ samuraiInfo, setSamuraiInfo }) {
                   }}
                   src={require(`../../gif/intro.gif`)}
                   alt="user-avatar"
-                ></img>
-              </div>
-
-            </>) : (<>
-
-              <h1>Пользователь {user.name}</h1>
-              <br></br>
-              <div className="user__wrapper">
-                <div className="user__wrapper-text">
-                  <div>Дата подписки: {moment(user.subDate).format('DD-MMM-YYYY HH:mm')}</div>
-                  <div>Email: {user.email}</div>
-                  <div>Тип темперамента: {user.temperament}</div>
-                  <div>Дата создания аккаунта: {moment(user.createdAt).format('DD-MMM-YYYY HH:mm')}</div>
-                  <div>Аватар: {user.avatar}</div>
-                </div>
-                <img
+                  animate={{ opacity: isSamurai ? 1 : 0 }}
+                ></motion.img>
+              ) : (
+                <motion.img
                   style={{
                     height: "400px",
                     width: "250px",
@@ -129,25 +125,14 @@ function User({ samuraiInfo, setSamuraiInfo }) {
                   }}
                   src={require(`../../images/${user.avatar}`)}
                   alt="user-avatar"
-                ></img>
-              </div>
-            </>)}
+                  animate={{ opacity: isSamurai ? 0 : 1 }}
+                ></motion.img>
+              )}
+            </div>
 
             <button type="button" onClick={handlePick} className="registerbtn">
               ДОБАВИТЬ ФОТО
             </button>
-
-            {/* {allImages == null
-              ? ""
-              : allImages.map((data) => {
-                  return (
-                    <img
-                      alt="user-images"
-                      key={data._id}
-                      src={require(`../images/${data.image}`)}
-                    />
-                  );
-                })} */}
 
             <input
               onChange={handleChange}
@@ -170,3 +155,4 @@ function User({ samuraiInfo, setSamuraiInfo }) {
 }
 
 export default User;
+
