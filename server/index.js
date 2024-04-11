91455272607fb91bfd166ac0ba2d40768ee13b95
strandpunk <strandpunk@yahoo.com>
2024-04-11 22:47:26 +0300

    reset https update 2

--- server/index.js
+++ server/index.js
@@ -1,6 +1,4 @@
 const express = require("express");
-const https = require("https");
-const fs = require("fs");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const mongoDB = require("./config/mongoDB");
@@ -13,23 +11,18 @@ const cors = require("cors");
 
 dotenv.config();
 
-// Загрузка SSL сертификата и приватного ключа
-const options = {
-  key: fs.readFileSync("./certificates/private.key"),
-  cert: fs.readFileSync("./certificates/certificate.pem")
-};
+// set up server
 
-// Установка HTTPS сервера
 const app = express();
 const PORT = process.env.PORT || 5000;
-const server = https.createServer(options, app);
-
-server.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
+const server = app.listen(PORT, () =>
+  console.log(`Server started on port: ${PORT}`)
+);
 
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: ["https://localhost:3000",  'https://192.168.56.1:3000'],
+    origin: "http://localhost:3000",
   },
 });
 
@@ -48,10 +41,17 @@ io.on("connection", (socket) => {
   });
 
   socket.on("new message", (newMessageReceived) => {
+    //var chat = newMessageReceived.chatId;
     console.log(newMessageReceived);
     console.log(1);
 
+    // if (!chat.users) return console.log("chat.users not defined");
+
+    // chat.users.forEach((user) => {
+    //   if (user._id == newMessageReceived.sender._id) return;
+
     socket.broadcast.emit("message received", newMessageReceived);
+    // });
   });
 });
 
@@ -59,16 +59,18 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["https://localhost:3000",  'https://192.168.56.1:3000'],
-    credentials: true,
+    origin: "http://localhost:3000",
+    credentials: true, //access-control-allow-credentials:true
     optionSuccessStatus: 200,
   })
 );
 
-// Подключение к MongoDB
+// connect to mongoDB
+
 mongoDB();
 
-// Настройка маршрутов
+// set up routes
+
 app.use("/auth", userRouter);
 app.use("/data", dataRouter);
 app.use("/chat", chatRouter);
