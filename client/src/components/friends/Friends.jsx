079e7dac445ae37b146dfa606f320354daf52946
strandpunk <strandpunk@yahoo.com>
2024-06-02 01:08:10 +0300

    update

--- client/src/components/friends/Friends.jsx
+++ client/src/components/friends/Friends.jsx
@@ -2,11 +2,32 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import "./UserList.scss";
 import { useNavigate } from "react-router-dom";
+const moment = require('moment');
 
 function Friends() {
   const [users, setUsers] = useState([]);
 
   const navigate = useNavigate()
+
+  
+  const calculateAge = (birthDate) => {
+    // Создаем объект Moment для текущей даты
+    const currentDate = moment();
+
+    // Создаем объект Moment для даты рождения
+    const birthMoment = moment(birthDate);
+
+    // Проверяем, является ли дата рождения позже текущей даты в текущем году
+    // Если да, то уменьшаем год на 1, чтобы получить корректный возраст
+    if (birthMoment.isAfter(currentDate)) {
+      birthMoment.subtract(1, 'years');
+    }
+
+    // Вычисляем разницу между текущей датой и датой рождения в годах
+    const ageInYears = currentDate.diff(birthMoment, 'years');
+
+    return ageInYears;
+  };
 
   async function getFriends() {
     const finded = await axios.get("http://localhost:5000/auth/get-friends");
@@ -35,7 +56,7 @@ function Friends() {
                 return (
                   <div className="user__card" key={data._id} onClick={() => openFriendProfile(data)}>
                     <div className="user__cardInfo">
-                      {data.name} <br />
+                      {data.name} {calculateAge(data.age)}<br />
                       {data.city}
                       <br />
                       {/* email: {data.email} */}
